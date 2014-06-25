50e5e2fa13bfbd99a7e413c4a31e0a254e06f60d
Jacinda Zhong <jacindaz@gmail.com>
2014-06-25 14:04:32 -0400

    FactoryGirl creates recipe objects with varying cooking times

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,10 @@
 class Recipe < ActiveRecord::Base
+  validates :name, presence: true
+  validates :instructions, presence: true
+  validates :cooking_time, presence: true, numericality: { greater_than: 0 }
+  validates :servings, presence: true, numericality: { greater_than: 0 }
+end
+
+def self.quick_recipes
+  where('cooking_time < ?', 30)
 end
--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,10 +2,10 @@
 
 FactoryGirl.define do
   factory :recipe do
-    name "MyString"
-    description ""
-    instructions ""
-    servings 1
-    cooking_time 1
+    name "Green Eggs & Ham"
+    description "Just what the title says."
+    instructions "1. Cook eggs.\n2. Cook ham.\n3. Combine"
+    servings 4
+    cooking_time 5
   end
 end
