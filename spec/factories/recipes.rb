91a00e2ef7c8b5fb620673db965a551092223b35
Jacinda Zhong <jacindaz@gmail.com>
2014-06-25 14:33:11 -0400

    Final push to github

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :recipe do
-    name "Green Eggs & Ham"
+    sequence(:name) { |n| "Green Eggs & Ham #{n}" }
     description "Just what the title says."
     instructions "1. Cook eggs.\n2. Cook ham.\n3. Combine"
     servings 4
