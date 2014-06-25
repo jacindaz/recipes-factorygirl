0bdbc987de7f15752ebf89cc745556dcf717ebf9
Jacinda Zhong <jacindaz@gmail.com>
2014-06-25 14:06:51 -0400

    FactoryGirl uses create_list to create multiple recipe objects with same attributes

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -4,16 +4,19 @@ RSpec.describe Recipe, :type => :model do
   describe '.quick_recipes' do
     it 'returns recipes with a cooking time less than 30 min' do
 
-      first_quick_recipe = FactoryGirl.create(:recipe, cooking_time: 5)
-      second_quick_recipe = FactoryGirl.create(:recipe, cooking_time: 4)
-      long_recipe = FactoryGirl.create(:recipe, cooking_time: 240)
+      quick_recipes = FactoryGirl.create_list(:recipe, 2, cooking_time: 5)
+      slow_recipes = FactoryGirl.create_list(:recipe, 3, cooking_time: 240)
 
       results = Recipe.quick_recipes
 
-      expect(results).to include(first_quick_recipe)
-      expect(results).to include(second_quick_recipe)
+      quick_recipes.each do |quick_recipe|
+        expect(results).to include(quick_recipe)
+      end
 
-      expect(results)to_not include(long_recipe)
+      slow_recipes.each do |slow_recipe|
+        expect(results).to_not include(slow_recipe)
+      end
+
     end
   end
 end
